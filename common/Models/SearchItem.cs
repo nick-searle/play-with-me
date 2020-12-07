161344bb3e102a8e812b26891bf2cdbd32e19a85
Nicholas Searle <nicholassearle@Nicholass-Mini.attlocal.net>
2020-12-07 11:12:25 -0500

    Break out classes

--- common/Models/SearchItem.cs
+++ common/Models/SearchItem.cs
@@ -6,12 +6,4 @@ namespace play_with_me.common.Models
         public string Title { get; set; }
         public bool Instock { get; set; }
     }
-
-    public class TargetSearchItem : SearchItem
-    {
-        public string Status { get; set; }
-        public string Tcin { get; set; }
-        public string Dpci { get; set; }
-        public string Upc { get; set; }
-    }
 }
---
+++ common/Models/TargetSearchItem.cs
@@ -0,0 +1,10 @@
+namespace play_with_me.common.Models
+{
+    public class TargetSearchItem : SearchItem
+    {
+        public string Status { get; set; }
+        public string Tcin { get; set; }
+        public string Dpci { get; set; }
+        public string Upc { get; set; }
+    }
+}
